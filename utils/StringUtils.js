65707ea64c336f0a2fc32a112483823c0ba6913d
XING <jackjohnton789@gmail.com>
2024-09-02 12:55:53 +0800

    add utils function

--- utils/StringUtils.js
+++ utils/StringUtils.js
@@ -1,1 +1,5 @@
-export class StringUtils {}
+export class StringUtils {
+    static ifPresentOrElse(text, replaceSymbol) {
+        return ObjectUtils.isExist(text) ? text : replaceSymbol;
+    }
+}
